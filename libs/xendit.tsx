edbbeaaf2894dec7c888628f82d260cfbfe72191
Arzivre <sonyfauzi@outlook.co.id>
2022-04-26 23:13:36 +0700

    fix type compilation error

--- libs/xendit.tsx
+++ libs/xendit.tsx
@@ -1,7 +1,7 @@
 import Xendit from 'xendit-node'
 
 const xendit = new Xendit({
-  secretKey: process.env.XENDIT_SECRET_KEY,
+  secretKey: process.env.XENDIT_SECRET_KEY as string,
 })
 
 export default xendit
