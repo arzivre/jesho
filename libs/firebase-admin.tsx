fafa649bff05da7af0b2e7ce087ece7a18c368c6
Arzivre <sonyfauzi@outlook.co.id>
2022-04-26 23:58:51 +0700

    use replace funct

--- libs/firebase-admin.tsx
+++ libs/firebase-admin.tsx
@@ -4,9 +4,9 @@ import * as admin from 'firebase-admin'
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      client_email: process.env.FIREBASE_CLIENT_EMAIL,
-      private_key: process.env.FIREBASE_PRIVATE_KEY,
       project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     }),
   })
 }
