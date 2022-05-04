fb5249db8a55f4ed59a8662085cf5fe3fff0e54a
Arzivre <sonyfauzi@outlook.co.id>
2022-05-04 21:09:34 +0700

    naming

---
+++ utils/post.ts
@@ -0,0 +1,11 @@
+export default async function post(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  })
+  return response.json() // parses JSON response into native JavaScript objects
+}
