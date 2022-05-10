15a4c929032370486e546714e76993accaabc724
Arzivre <sonyfauzi@outlook.co.id>
2022-05-10 11:01:23 +0700

    fix api order

--- pages/api/user/[order].ts
+++ pages/api/user/[order].ts
@@ -1,16 +1,27 @@
-import { NextApiRequest,NextApiResponse } from 'next'
-import { adminCollection } from 'libs/adminGetCollection'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { db } from 'libs/firebase-admin'
 
-export default async function handler(req:NextApiRequest, res:NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { order } = req.query
+  //* GET
   if (req.method === 'GET') {
     try {
-      const { results } = await adminCollection(
-        'orders',
-        ['userId', '==', order],
-        ['createdAt', 'desc'],
-        5
-      )
+      const snapshot = await db
+        .collection('orders')
+        .where('userId', '==', order)
+        .orderBy('createdAt', 'desc')
+        .limit(5)
+        .get()
+
+      let results: any[] = []
+
+      snapshot.forEach((doc) => {
+        results.push({ id: doc.id, ...doc.data() })
+      })
+
       res.status(200).json(results)
     } catch (error) {
       res.status(500).json({ error })
