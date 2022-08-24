2790e7e05822bfc2ced78c4fe6f48cbfc318140b
Arzivre <sonyfauzi@outlook.co.id>
2022-08-24 13:43:29 +0700

    update page

--- pages/harga.tsx
+++ pages/harga.tsx
@@ -4,8 +4,11 @@ import Meta from 'components/Meta'
 const Harga = () => {
   return (
     <>
-      <Meta title='Daftar Harga - Jesho'/>
-      <Main>Daftar Harga</Main>
+      <Meta title='Daftar Harga - Jesho' />
+      <Main>
+        <h1 className='mt-12 px-4 text-center text-5xl'>Daftar Harga</h1>
+        <main className='mx-auto min-h-screen max-w-screen-md'>...</main>
+      </Main>
     </>
   )
 }
