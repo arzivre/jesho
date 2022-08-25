a411b6efcbe89ace497e82d1f37ad8c152320a47
Arzivre <sonyfauzi@outlook.co.id>
2022-08-25 10:08:45 +0700

    testimoni content

--- pages/testimoni.tsx
+++ pages/testimoni.tsx
@@ -6,11 +6,33 @@ const Testimon = () => {
     <>
       <Meta title='Testimoni - Jesho' />
       <Main>
-        <h1 className='px-4 text-center text-5xl mt-12'>Testimoni</h1>
-        <main className='min-h-screen mx-auto max-w-screen-md'>
-          <div>
-            
-          </div>
+        <h1 className='mt-12 px-4 text-center text-5xl'>Testimoni</h1>
+        <main className='mx-auto min-h-screen max-w-screen-md'>
+          <section>
+            <ol className='mt-12 flex gap-8 text-xl [&>li]:border [&>li]:p-4 [&>li>p]:mb-4 [&>li:hover]:shadow'>
+              <li>
+                <p>
+                  Hasilnya bagus, pengiriman cepat. saya order h-6 lebaran h-2
+                  lebaran sudah dikirim. terima kasih Jesho.
+                </p>
+                <span>- Heri -</span>
+              </li>
+              <li>
+                <p>
+                  Bagus banget setelah dipasang, sesuai pesanan saya. Seller
+                  sabar dan sangat membantu proses pembuatan, puas sekali.
+                </p>
+                <span>- Prisci Lia -</span>
+              </li>
+              <li>
+                <p>
+                  Mantap Sekali, Saya order nomer meja 200pc 6 hari sudah jadi,
+                  saya pikir akan lama sampai 3 minggu, ternyata Cepat.
+                </p>
+                <span>- Ryan -</span>
+              </li>
+            </ol>
+          </section>
         </main>
       </Main>
     </>
